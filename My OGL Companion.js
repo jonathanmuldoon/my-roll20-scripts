8104ebc394e013c5f930e311a3f65e5ded22b0fd
jonathanmuldoon <jonathanmuldoon@users.noreply.github.com>
2017-05-05 00:20:04 -0500

    Updated to 0.0.2

--- My OGL Companion.js
+++ My OGL Companion.js
@@ -1,77 +1,132 @@
+var MyOGLCompanion_verson = "0.0.2";
+
+var img = "background-image: -webkit-linear-gradient(left, #76ADD6 0%, #a7c7dc 100%);";
+var tshadow = "text-shadow: -1px -1px #000, 1px -1px #000, -1px 1px #000, 1px 1px #000 , 2px 2px #222;";
+var boxStyle= "width: 250px; border-radius: 8px 8px 8px 8px; padding: 5px; font-size: 10pt; " + tshadow + " box-shadow: 3px 3px 1px #707070; " + img + " color:#FFF; border:2px solid black; text-align:center; vertical-align:middle;"
+var buttonStyle = "border: 1px solid black; margin: 1px; background-color: #588a9e; border-radius: 4px;  box-shadow: 1px 1px 1px #707070;";
+
+var getParams = function(commandLine) {
+
+    var result ={};
+    var args = _.rest(commandLine.split("--"));
+    _.each(args, function(token) {
+        var array = token.split('=');
+        array[1] && (result[array[0].trim()] = array[1].trim());
+    });
+    return result;
+};
+    
+var getCharacter = function(msg) {
+
+    var character ={};
+    var params = getParams(msg.content);
+    
+    if (params.characterName != undefined) {
+        character = findObjs({
+            _type: "character",
+            name: params.characterName})[0];
+    } else if (msg.selected != undefined) {
+        var selectedObject =  getObj(msg.selected[0]._type, msg.selected[0]._id);
+        character = getObj('character', selectedObject.get('represents'));
+    }
+    
+    return character;
+};    
+    
 on("chat:message", function(msg) {
     
-    function getArgs(command){
-        return;
-    }
     if (msg.type == "api") {
         
-        if (msg.content.startsWith("!AdvToggle") && msg.selected != undefined) {
+        if (msg.content.startsWith("!AdvToggle ")) {
+            
             var normal = "{{query=1}} {{normal=1}} {{r2=[[0d20";
             var advantage = "{{query=1}} {{advantage=1}} {{r2=[[1d20";
             var disadvantage = "{{query=1}} {{disadvantage=1}} {{r2=[[1d20";
-            
-            var setAdvTo= msg.content.split("--")[1];
-            log (setAdvTo);
-            
-            var selectedObject =  getObj(msg.selected[0]._type, msg.selected[0]._id);
-            var character = getObj('character', selectedObject.get('represents'));
-            log(character);
-            log (character.id);
+
+            var character = getCharacter(msg);
 
             var AdvToggle = findObjs({
                 name: "advantagetoggle",
                 _type:"attribute",
                 _characterid:character.id})[0];
-            log(AdvToggle);
-            log (AdvToggle.get("current"));
-            //var current = /{{query=1}} {{(\w+)=1}}/.exec(AdvToggle.get("current"))[1];
-            if (setAdvTo == "normal"){
-                AdvToggle.set("current",normal);
-            } else if (setAdvTo == "advantage"){
-                AdvToggle.set("current",advantage);
-            } else if (setAdvTo == "disadvantage"){
-                AdvToggle.set("current",disadvantage);
-            }
+               
+            var setAdvTo = getParams(msg.content).setting; 
             
-            log (AdvToggle.get("current"));         
+            if (AdvToggle != undefined){
+                if (setAdvTo == "normal"){
+                    AdvToggle.set("current",normal);
+                } else if (setAdvTo == "advantage"){
+                    AdvToggle.set("current",advantage);
+                } else if (setAdvTo == "disadvantage"){
+                    AdvToggle.set("current",disadvantage);
+                }
+            }
         }
         
-        if (msg.content.startsWith("!SELECTED") && msg.selected != undefined) { 
-            _.each(msg.selected, function(s) {
-                log(s);
-            });
-        }
-        
-        if (msg.content.startsWith("!SavingThrows") && msg.selected != undefined) {
-            var selectedObject =  getObj(msg.selected[0]._type, msg.selected[0]._id);
-            //log (selectedObject);
-            var selectedChar = getObj('character', selectedObject.get('represents'));
-            //log (selectedChar);
-            var charName = selectedChar.get("name");
-            //log (charName);  
+        if (msg.content.startsWith("!SavingThrows")) {
+            
+            var params = getParams(msg.content);
+            var charName = getCharacter(msg).get("name");
+            
+            var thisButtonStyle = buttonStyle + "font-size: 8pt; width: 60px;'"; 
 
-            var buttonStyle = "style='font-size: 9pt; width: 25px; border: 1px solid black; margin: 1px; background-color: #588a9e; border-radius: 4px;  box-shadow: 1px 1px 1px #707070;'";
-            //if (PC) --> This block only works for PCs
-            sendChat(msg.who, "/w " + msg.who + " <div class='sheet-rolltemplate-simple'><div class='sheet-container' style='border-radius: 0px;'><div class='sheet-label' style='margin-top:5px;'><span style='display:block;'>" +
-                charName + "'s <br/> Saving Throws <br/>" +
-                "<a " + buttonStyle + " href='~"+ charName +"|strength_save'> Str </a> | " +
-                "<a " + buttonStyle + " href='~"+ charName +"|dexterity_save'> Dex </a> | " +
-                "<a " + buttonStyle + " href='~"+ charName +"|constitution_save'> Con </a><br />" +
-                "<a " + buttonStyle + " href='~"+ charName +"|intelligence_save'> Int </a> | " +
-                "<a " + buttonStyle + " href='~"+ charName +"|wisdom_save'> Wis </a> | " +
-                "<a " + buttonStyle + " href='~"+ charName +"|charisma_save'> Cha </a> " +
+            sendChat(msg.who, "/w " + msg.who + " <div style='" + boxStyle + "'>" +
+                "<b>"+ charName + "'s Saving&nbsp;Throws</b><br/>" +
+                "<a style='" + thisButtonStyle + "' href='~"+ charName +"|strength_save'> Strength </a> | " +
+                "<a style='" + thisButtonStyle + "' href='~"+ charName +"|dexterity_save'> Dexterity </a> | " +
+                "<a style='" + thisButtonStyle + "' href='~"+ charName +"|constitution_save'> Constitution </a><br />" +
+                "<a style='" + thisButtonStyle + "' href='~"+ charName +"|intelligence_save'> Intelligence </a> | " +
+                "<a style='" + thisButtonStyle + "' href='~"+ charName +"|wisdom_save'> Wisdom </a> | " +
+                "<a style='" + thisButtonStyle + "' href='~"+ charName +"|charisma_save'> Charisma </a> " +
                 "</span></div></div></div>");
+        }
+        
+        if (msg.content.startsWith("!AbilityChecks")) {
+            
+            var params = getParams(msg.content);
+            var charName = getCharacter(msg).get("name");
+            
+            var thisButtonStyle = buttonStyle + "font-size: 8pt; width: 60px;"; 
 
-            //else if (NPC) --> Need a separate block for NPCs
-            /*    sendChat(msg.who, "/w " + msg.who + " <div class='sheet-rolltemplate-simple'><div class='sheet-container' style='border-radius: 0px;'><div class='sheet-label' style='margin-top:5px;'><span style='display:block;'>" +
-                charName + "'s <br/> Saving Throws <br/>" +
-                "<a " + buttonStyle + " href='~"+ charName +"|npc_str_save'> Str </a> | " +
-                "<a " + buttonStyle + " href='~"+ charName +"|npc_dex_save'> Dex </a> | " +
-                "<a " + buttonStyle + " href='~"+ charName +"|npc_constitution_save'> Con </a><br />" +
-                "<a " + buttonStyle + " href='~"+ charName +"|npc_intelligence_save'> Int </a> | " +
-                "<a " + buttonStyle + " href='~"+ charName +"|npc_wisdom_save'> Wis </a> | " +
-                "<a " + buttonStyle + " href='~"+ charName +"|npc_charisma_save'> Cha </a> " +
-                "</span></div></div></div>");                */
+            sendChat(msg.who, "/w " + msg.who + " <div style='" + boxStyle + "'>" +
+                "<b>"+ charName + "'s Ability&nbsp;Checks </b><br/>" +
+                "<a style='" + thisButtonStyle + "' href='~"+ charName +"|strength'> Strength </a> | " +
+                "<a style='" + thisButtonStyle + "' href='~"+ charName +"|dexterity'> Dexterity </a> | " +
+                "<a style='" + thisButtonStyle + "' href='~"+ charName +"|constitution'> Constitution </a><br />" +
+                "<a style='" + thisButtonStyle + "' href='~"+ charName +"|intelligence'> Intelligence </a> | " +
+                "<a style='" + thisButtonStyle + "' href='~"+ charName +"|wisdom'> Wisdom </a> | " +
+                "<a style='" + thisButtonStyle + "' href='~"+ charName +"|charisma'> Charisma </a> " +
+                "</span></div></div></div>");
+        }
+        
+        if (msg.content.startsWith("!SkillChecks")) {
+            
+            var params = getParams(msg.content);
+            var charName = getCharacter(msg).get("name");
+            
+            var thisButtonStyle = buttonStyle + "font-size: 8pt; width: 65px;"; 
+            
+            sendChat(msg.who, "/w " + msg.who + " <div style='" + boxStyle + "'>" +
+                "<b>"+ charName + "'s Skill&nbsp;Checks</b><br/>" +
+                "<a style='" + thisButtonStyle + "' href='~"+ charName +"|acrobatics'> Acrobatics </a> " +
+                "<a style='" + thisButtonStyle + "' href='~"+ charName +"|animal_handling'> Animal H. </a> " +
+                "<a style='" + thisButtonStyle + "' href='~"+ charName +"|arcana'> Arcana </a> <br />  " +
+                "<a style='" + thisButtonStyle + "' href='~"+ charName +"|athletics'> Athletics </a> " +
+                "<a style='" + thisButtonStyle + "' href='~"+ charName +"|deception'> Deception </a> " +
+                "<a style='" + thisButtonStyle + "' href='~"+ charName +"|history'> History </a> <br />" +
+                "<a style='" + thisButtonStyle + "' href='~"+ charName +"|insight'> Insight </a> " +
+                "<a style='" + thisButtonStyle + "' href='~"+ charName +"|intimidation'> Intimidation </a> " +
+                "<a style='" + thisButtonStyle + "' href='~"+ charName +"|investigation'> Investigation </a> <br /> " +
+                "<a style='" + thisButtonStyle + "' href='~"+ charName +"|medicine'> Medicine </a> " +
+                "<a style='" + thisButtonStyle + "' href='~"+ charName +"|nature'> Nature </a> " +
+                "<a style='" + thisButtonStyle + "' href='~"+ charName +"|perception'> Perception </a> <br />" +
+                "<a style='" + thisButtonStyle + "' href='~"+ charName +"|performance'> Performance </a> " +
+                "<a style='" + thisButtonStyle + "' href='~"+ charName +"|persuasion'> Persuasion </a> " +
+                "<a style='" + thisButtonStyle + "' href='~"+ charName +"|religion'> Religion </a> <br />  " +
+                "<a style='" + thisButtonStyle + "' href='~"+ charName +"|sleight_of_hand'> Sleight/Hand </a>  " +
+                "<a style='" + thisButtonStyle + "' href='~"+ charName +"|stealth'> Stealth </a> " +
+                "<a style='" + thisButtonStyle + "' href='~"+ charName +"|survival'> Survival </a> <br />" +                
+                "</span></div></div></div>");
         }
     }
 });
